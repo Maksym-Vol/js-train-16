77cd8f3beb0843ed392b7ef7f76f805c0d335079
p1 <you@example.com>
2023-09-05 15:36:41 +0300

    done

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ function filterStudentsByGrade(students, grade) {
   // Відфільтруємо тільки тих студентів оцінка яких співпадає з grade
   // За допомогою перебору масиву виведемо повідомлення для відстеження роботи програми з іменем кожного студента,
   //  який має необхідну оцінку
+  console.error("Завдання: 1 ==============================");
+  const filteredStudents = students.filter(student => student.grade === grade);
+  filteredStudents.forEach(student => {
+    console.log(`Ім'я: ${student.name}, Оцінка: ${student.grade}`);
+  });
 }
 
 // Виклик функції для фільтрації студентів з рівнем "A"
@@ -38,6 +43,14 @@ function logArrayElements(arr) {
   // Повертаємо null, що свідчить про неможливість обробки вхідних даних.
   // Перебираємо кожен елемент масиву.
   // Виводимо в консоль поточний елемент масиву та його індекс в форматі Елемент <індекс>: <значення>
+  if (!Array.isArray(arr)) {
+    console.error("Аргумент має бути масивом!")
+    return null;
+  }
+  for (let i = 0; i < arr.length; i++) {
+   console.log(`Елемент ${i}: ${arr[i]}`);
+  }
+  
 }
 
 // Перевірка
@@ -65,6 +78,12 @@ function warnAboutMaxArrayLength(arr, maxLength) {
   // Перевіряємо, чи довжина масиву перевищує максимальну допустиму довжину.
   // Якщо довжина масиву перевищує максимальну, виводимо попередження про потенційну проблеми з текстом: "Увага! Довжина масиву перевищує максимально допустиму!".
   // Якщо довжина масиву не перевищує максимальну, виведеме в консоль корисне повідомлення з текстом: "Довжина масиву не перевищує максимально допустиму.".
+  if (!Array.isArray(arr) || typeof maxLength !== 'number') {
+    console.error("Перший аргумент має бути масивом, другий аргумент має бути числом!");
+    return null;
+  }
+  arr.length > maxLength ? console.warn("Увага! Довжина масиву перевищує максимально допустиму!") :
+    console.info("Довжина масиву не перевищує максимально допустиму.");
 }
 
 // Перевірка
@@ -84,6 +103,11 @@ function compareArrays(arr1, arr2) {
   // Сортуємо масиви за зростанням.
   // Оскільки напряму порівняти масиви за допомогою оператора === не можно, перетворимо ії в json і порівняємо
   // Використовуємо `console.assert()`, щоб вивести помилку, якщо масиви не однакові.
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  const arr1JSON = JSON.stringify(arr1);
+  const arr2JSON = JSON.stringify(arr2);
+  console.assert(arr1JSON === arr2JSON, "Масиви не однакові!");
 }
 
 console.log("Завдання: 4 ==============================");
@@ -110,6 +134,21 @@ function buildMatrix(size) {
   // Додаємо рядок до матриці.
   // Виводимо матрицю в консоль за допомогою як таблицю.
   // Повертаємо створену матрицю.
+  if (typeof size !== 'number') {
+        console.error("Аргумент має бути числом!");
+    return null;
+  }
+  let matrix = [];
+  for (let i = 0; i < size; i++) {
+    let row = [];
+    for (let j = 0; j < size; j++) {
+      const randomNum = Math.floor(Math.random() * 10);
+      row.push(randomNum);
+    }
+    matrix.push(row);
+  }
+  console.table(matrix);
+  return matrix;
 }
 
 console.log("Завдання: 5 ==============================");
@@ -127,6 +166,12 @@ function traceBackward(n) {
   // Виводимо поточне число n.
   // Якщо n є більше 1, викликаємо функцію traceBackward знову з аргументом n - 1.
   // Виведемо стек викликів, коли досягнемо 1.
+  console.log(n);
+  if (n === 1) {
+    return;
+  }
+  traceBackward(n - 1);
+  console.trace();
 }
 
 console.log("Завдання: 6 ==============================");
@@ -144,6 +189,12 @@ function displayGroupedInfo(students) {
   // Виведемо корисне повідомлення з текстом `Вік: ${student.age}`
   // Виведемо корисне повідомлення з текстом `Предмет: ${student.subject}`
   // Закриємо групу
+  for (const student of students) {
+    console.group(`Студент: ${students.name}`);
+    console.info(`Вік: ${student.age}`);
+     console.info(`Предмет: ${student.subject}`);
+    console.groupEnd();
+  }
 }
 
 console.log("Завдання: 7 ==============================");
